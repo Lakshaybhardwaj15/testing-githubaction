2c5a28506c022cce9e433b55ed46aaceee7577b3
lakshay <bhardwajlakshay238@gmail.com>
2025-04-24 17:06:33 +0530

    updated

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="funcAppGitAction")
 def funcAppGitAction(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('hii!!! Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
     if not name:
@@ -20,6 +20,6 @@ def funcAppGitAction(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "Pass a name in the query string or in the request body for a personalized response.",
+             "hiiii Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
